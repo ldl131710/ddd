c86351ea581f77f12beda86a5d85b4ef495a1496
ldl <ldl131710@163.comm>
2019-07-11 23:20:26 +0800

    方法

--- src/main/java/com/example/ddd/DddApplication.java
+++ src/main/java/com/example/ddd/DddApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DddApplication {
 
+    //在版本1上的修改
     public static void main(String[] args) {
         SpringApplication.run(DddApplication.class, args);
     }
